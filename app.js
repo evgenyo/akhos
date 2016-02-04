b172bf9adeb9319e55a9c455d5e9dfe535adc8e1
Evgeny <evgenyo@yahoo.com>
2016-02-04 04:53:04 -0800

    fixed DBs issue

--- app.js
+++ app.js
@@ -10,20 +10,21 @@ var app = express();
 var mongoose = require('mongoose');
 var passport = require('passport');
 
-var database = require('./config/database');
+// var database = require('./config/database');
 
-if (app.get('env') === 'development') {
-    mongoose.connect('mongodb://localhost/akhos-dev', function(err, db) {
-        if (!err) {
-            console.log('Connected to mongodb!');
-        } else {
-            console.dir(err);
-        }
-    });
-} else {
-    mongoose.connect(database.url);
-}
+// if (app.get('env') === 'development') {
+//     mongoose.connect('mongodb://localhost/akhos-dev', function(err, db) {
+//         if (!err) {
+//             console.log('Connected to mongodb!');
+//         } else {
+//             console.dir(err);
+//         }
+//     });
+// } else {
+//     mongoose.connect(database.url);
+// }
 
+mongoose.connect("mongodb://evgenyo:letitwork@ds055515.mongolab.com:55515/akhos-production");
 
 require('./models/Posts');
 require('./models/Comments');
